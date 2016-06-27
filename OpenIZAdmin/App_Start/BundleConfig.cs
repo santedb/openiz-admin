b81a94ab4d56ce73204f279dd565d23203eb5311
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-06-27 17:38:14 -0400

    added moment

--- OpenIZAdmin/App_Start/BundleConfig.cs
+++ OpenIZAdmin/App_Start/BundleConfig.cs
@@ -41,6 +41,9 @@ namespace OpenIZAdmin
 					  "~/Scripts/bootstrap.js",
 					  "~/Scripts/respond.js"));
 
+			bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+					  "~/Scripts/moment.min.js"));
+
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
 					  "~/Content/site.css"));
