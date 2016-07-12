b5edf6312e6f2d58d7f5eaa17da42acc8916857d
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-07-11 20:03:57 -0400

    removed modernizer

--- OpenIZAdmin/App_Start/BundleConfig.cs
+++ OpenIZAdmin/App_Start/BundleConfig.cs
@@ -45,11 +45,6 @@ namespace OpenIZAdmin
 						"~/Scripts/toastr.min.js",
 						"~/Scripts/toastr-logic.js"));
 
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
-
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js",
 					  "~/Scripts/respond.js"));
