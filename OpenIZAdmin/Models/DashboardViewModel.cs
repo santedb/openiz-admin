83b9befa39370400b2739831d3e6ba7fc315beaf
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-07-13 23:44:13 -0400

    Model code cleanup

--- OpenIZAdmin/Models/DashboardViewModel.cs
+++ OpenIZAdmin/Models/DashboardViewModel.cs
@@ -1,29 +1,27 @@
 ﻿/*
  * Copyright 2016-2016 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: Nityan
  * Date: 2016-7-8
  */
+
 using OpenIZAdmin.Models.AppletModels.ViewModels;
 using OpenIZAdmin.Models.CertificateModels.ViewModels;
 using OpenIZAdmin.Models.DeviceModels.ViewModels;
 using OpenIZAdmin.Models.UserAdministration.ViewModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace OpenIZAdmin.Models
 {
@@ -31,7 +29,6 @@ namespace OpenIZAdmin.Models
 	{
 		public DashboardViewModel()
 		{
-
 		}
 
 		public IEnumerable<AppletViewModel> Applets { get; set; }
@@ -43,6 +40,5 @@ namespace OpenIZAdmin.Models
 		public IEnumerable<UserRoleViewModel> UserRoles { get; set; }
 
 		public IEnumerable<UserViewModel> Users { get; set; }
-
 	}
 }
