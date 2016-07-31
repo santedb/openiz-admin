631d5c4c90f9c16fd13333402bb32c2894f5973b
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-07-31 19:55:57 -0400

    code cleanup using codemaid

--- OpenIZAdmin/Util/UserUtil.cs
+++ OpenIZAdmin/Util/UserUtil.cs
@@ -1,36 +1,30 @@
 ﻿/*
  * Copyright 2016-2016 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: Nityan
  * Date: 2016-7-30
  */
-using System;
+
+using OpenIZ.Core.Model.AMI.Auth;
 using OpenIZ.Messaging.AMI.Client;
-using OpenIZAdmin.Models.RoleModels.ViewModels;
+using OpenIZAdmin.Models.UserModels;
+using OpenIZAdmin.Models.UserModels.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Web;
-using OpenIZAdmin.Models.UserModels.ViewModels;
-using OpenIZ.Core.Model.AMI.Auth;
-using OpenIZAdmin.Models.UserModels;
-using OpenIZ.Core.Model.Security;
-using OpenIZ.Core.Model.Entities;
-using OpenIZAdmin.Extensions;
-using OpenIZ.Core.Model.Constants;
 
 namespace OpenIZAdmin.Util
 {
