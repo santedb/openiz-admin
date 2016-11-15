df80727aceb6eb56aab5fb27bd725e34e7e67e7a
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-11-14 21:22:11 -0500

    starting to add basic unit tests for class mappings

--- OpenIZAdmin.Tests/Util/AlertUtilTests.cs
+++ OpenIZAdmin.Tests/Util/AlertUtilTests.cs
@@ -14,33 +14,41 @@
  * the License.
  * 
  * User: khannan
- * Date: 2016-5-31
+ * Date: 2016-11-14
  */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenIZAdmin.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenIZAdmin;
-using OpenIZAdmin.Controllers;
+using System.Threading.Tasks;
 
-namespace OpenIZAdmin.Tests.Controllers
+namespace OpenIZAdmin.Tests.Util
 {
 	[TestClass]
-	public class HomeControllerTest
+	public class AlertUtilTests
 	{
+
 		[TestMethod]
-		public void Index()
+		public void TestAlertMap()
 		{
-			// Arrange
-			HomeController controller = new HomeController();
+			var alertId = Guid.Parse("12186EC5-C5A7-4D6E-94B5-0F77C152790A");
+			var alertCreatedBy = Guid.Parse("BCE8DC56-BCC5-4CBF-8649-C24F14606BF7");
 
-			// Act
-			ViewResult result = controller.Index() as ViewResult;
+			var actual = AlertUtil.ToAlertMessageInfo(new Models.AlertModels.ViewModels.AlertViewModel
+			{
+				Body = "body",
+				CreatedBy = alertCreatedBy,
+				Flags = Models.AlertModels.AlertMessageFlags.Acknowledged,
+				From = "from",
+				Id = alertId,
+				Subject = "subject",
+				Time = new DateTime(1970, 01, 01),
+				To = "to"
+			});
 
-			// Assert
-			Assert.IsNotNull(result);
+			Assert.AreEqual("body", actual.AlertMessage.Body);
 		}
 	}
 }
