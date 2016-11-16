c01e94608076209da598c18f10a2580ee926ce16
Rory Yendt <rory.yendt@mohawkcollege.ca>
2016-11-16 11:05:48 -0500

    Added edit concept page

---
+++ OpenIZAdmin/Models/ConceptModels/EditConceptModel.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Copyright 2016-2016 Mohawk College of Applied Arts and Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * User: khannan
+ * Date: 2016-8-14
+ */
+
+using OpenIZ.Core.Model.DataTypes;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace OpenIZAdmin.Models.ConceptModels
+{
+    public class EditConceptModel
+    {
+        public EditConceptModel()
+        {
+            this.LanguageList = new List<SelectListItem>();
+        }
+
+        [Display(Name = "Language", ResourceType = typeof(Localization.Locale))]
+        [Required(ErrorMessageResourceName = "LanguageRequired", ErrorMessageResourceType = typeof(Localization.Locale))]
+        public string Language { get; set; }
+
+        public List<SelectListItem> LanguageList { get; set; }
+
+        [Display(Name = "Mnemonic", ResourceType = typeof(Localization.Locale))]
+        [Required(ErrorMessageResourceName = "MnemonicRequired", ErrorMessageResourceType = typeof(Localization.Locale))]
+        [StringLength(255, ErrorMessageResourceName = "MnemonicTooLong", ErrorMessageResourceType = typeof(Localization.Locale))]
+        public string Mnemonic { get; set; }
+
+        [Display(Name = "Name", ResourceType = typeof(Localization.Locale))]
+        [Required(ErrorMessageResourceName = "NameRequired", ErrorMessageResourceType = typeof(Localization.Locale))]
+        [StringLength(255, ErrorMessageResourceName = "NameTooLong", ErrorMessageResourceType = typeof(Localization.Locale))]
+        public string Name { get; set; }
+    }
+}
